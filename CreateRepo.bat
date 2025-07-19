0929c21e434b4c9550922a7e465c4c7050ff6e52
GR RAJU <rajug792@gmail.com>
2025-07-20 01:01:46 +0530

    Added semantic <main> structure across layout files

---
+++ CreateRepo.bat
@@ -0,0 +1,7 @@
+echo "# elitor" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/rajug792/elitor.git
+git push -u origin main
---
+++ createpushrepo.bat
@@ -0,0 +1,3 @@
+git remote add origin https://github.com/rajug792/elitor.git
+git branch -M main
+git push -u origin main
